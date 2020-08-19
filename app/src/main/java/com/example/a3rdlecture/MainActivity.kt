fbefda13ed735da5950d64fb5eaca09b62799bc1
DAFigueiredo <69812826+DAFigueiredo@users.noreply.github.com>
2020-08-19 17:57:07 +0100

    Update MainActivity.kt
    variables

--- app/src/main/java/com/example/a3rdlecture/MainActivity.kt
+++ app/src/main/java/com/example/a3rdlecture/MainActivity.kt
@@ -7,5 +7,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //variables
+
+        val x = 5
+        val y = 4
+
+        println(x * y)
+
+        val age = 20
+
+        val result = age * 5 / 4
+
+        println(result)
+
+
     }
 }
