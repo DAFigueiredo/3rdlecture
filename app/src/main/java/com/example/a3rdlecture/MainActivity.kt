c7d849182d83a5cf4ee569aa4d9043353c170240
DAFigueiredo <69812826+DAFigueiredo@users.noreply.github.com>
2020-08-19 19:47:27 +0100

    Update MainActivity.kt
    swtich

--- app/src/main/java/com/example/a3rdlecture/MainActivity.kt
+++ app/src/main/java/com/example/a3rdlecture/MainActivity.kt
@@ -152,5 +152,37 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        //Switch
+
+        val day = 6
+        var dayString = ""
+
+        if (day == 1) {
+            dayString = "Monday"
+        } else if (day == 2) {
+            dayString = "Tuesday"
+        } else if (day == 3) {
+            dayString = "Wednesday"
+        } else if (day == 4) {
+            dayString = "Thursday"
+        } else if (day == 5){
+            dayString = "Friday"
+        }
+
+        when (day) {
+
+            1 -> dayString = "Monday"
+            2 -> dayString = "Tuesday"
+            3 -> dayString = "Wednesday"
+            4 -> dayString = "Thursday"
+            5 -> dayString = "Friday"
+            else -> dayString ="Weekend"
+
+        }
+
+        println(dayString)
+
+
+
     }
 }
